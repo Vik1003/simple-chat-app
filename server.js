ec1280e279742936f61cb62757e6df9402b72d81
vikram <vikrammohanraj1000@gmail.com>
2023-08-25 00:01:38 +0530

    dcbdc

--- server.js
+++ server.js
@@ -1,26 +1,62 @@
-const express = require('express');
+
+// const http = require('http');
+// const express = require('express');
+// const socketIO = require('socket.io');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIO(server);
+
+// // Serve your static files (HTML, CSS, etc.)
+// app.use(express.static(__dirname + '/public'));
+
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
+
+//     // Listen for incoming messages
+//     socket.on('chat message', (message) => {
+//         // Broadcast the message to all connected clients
+//         io.emit('chat message', message);
+//     });
+
+//     // Handle disconnect
+//     socket.on('disconnect', () => {
+//         console.log('A user disconnected');
+//     });
+// });
+
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => {
+//     console.log('Server is running on  http://localhost:5000');
+// });
+//-----------------------------------------------------------------------------------
 const http = require('http');
-const socketIo = require('socket.io');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIO(server);
 
+// Serve your static files (HTML, CSS, etc.)
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+    console.log('A user connected');
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+    // Listen for incoming messages
+    socket.on('chat message', (message) => {
+        // Broadcast the message to all connected clients
+        io.emit('chat message', message);
+    });
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+    // Handle disconnect
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
-const PORT =  8000; //process.env.PORT ||
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log('Server is running on http://localhost:8000');
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
